f84ffe2090338b8de256e09ea54d307b3dafc42c
Sarmistha_Jena <sarmistha_jena@epam.com>
2024-01-24 16:33:17 +0530

    Initial commit

---
+++ SetUp/main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hasicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-south-1"
+}
+
+#Create target machine
+resource "aws_instance" "ansible_node" {
+  count                       = var.nodeCount
+  ami                         = var.amiType
+  instance_type               = var.instanceType
+  key_name                    = var.pemFile
+  associate_public_ip_address = true
+  user_data                   = <<-EOF
+               #!/bin/bash
+               sudo apt update
+               sudo apt install -y python3
+              EOF
+  tags                        = {
+    Name = "ANSIBLE TARGET NODE"
+  }
+}
+
+#Create inventory file
+resource "local_file" "ansible_inventory" {
+  content = templatefile("./templates/hosts.tpl",
+    {
+      keyfile     = var.pemFile
+      demoservers = aws_instance.ansible_node.*.public_ip
+    })
+  filename = "./ansible/host.cfg"
+}
+
+#Create ansible controller
+resource "aws_instance" "ansible_controller" {
+  ami                         = var.amiType
+  instance_type               = var.instanceType
+  key_name                    = var.pemFile
+  associate_public_ip_address = true
+  user_data                   = <<-EOF
+               #!/bin/bash
+               sudo apt update
+               sudo apt install -y ansible
+              EOF
+  depends_on                  = [aws_instance.ansible_node]
+  tags                        = {
+    Name = "ANSIBLE CONTROLLER"
+  }
+  provisioner "file" {
+    source      = "./ansible/host.cfg"
+    destination = "/home/ubuntu/host.cfg"
+  }
+  provisioner "file" {
+    source      = "./${var.pemFile}.pem"
+    destination = "/home/ubuntu/${var.pemFile}.pem"
+  }
+  provisioner "remote-exec" {
+    inline = ["chmod 400 /home/ubuntu/${var.pemFile}.pem"]
+  }
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("${var.pemFile}.pem")
+    host        = self.public_ip
+  }
+}
---
+++ SetUp/var.tf
@@ -0,0 +1,12 @@
+variable "amiType" {
+  default = "ami-0ef82eeba2c7a0eeb"
+}
+variable "instanceType" {
+  default = "t2.micro"
+}
+variable "nodeCount" {
+  default = "1"
+}
+variable "pemFile" {
+  default = "linux-pract1"
+}
